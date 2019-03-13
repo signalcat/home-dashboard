38f0b1a76cbc33ddfb748a66cab329ff6b2e22a1
He Zhang <rbczhang90@gmail.com>
2019-03-13 15:08:53 -0700

    Add add new plant button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
-import PlantsBoard from './containers/plantsBoard';
+import PlantsBoard from './containers/PlantsBoard';
 
 class App extends Component {
   render() {
--- src/containers/plantsBoard.js
+++ src/containers/plantsBoard.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
 import Aux from '../hoc/Aux/Aux';
-import PlantsList from '../components/Plants/PlantsList';
-import PlantsControls from '../components/Controls/PlantsControls';
+import classes from './PlantsBoard.module.css'
+import Plant from '../components/Plants/Plant';
+
 
 class PlantsBoard extends Component {
 
-    state = {
-        plants: {
-            aloe: 1,
-            pepper: 2
+    constructor(props) {
+        super(props);
+        this.state = {
+            plants: {
+                aloe: 1,
+                pepper: 2
+            }
         }
     }
-
+    
     addPlantHandler = () => {
-        const updatedPlants = {...this.state.plants};
-        updatedPlants["newPlant"] = 0;
-        this.setState({plants: updatedPlants});
+        const object = this.state.plants;
+        let newPlant = this.refs.input.value;
+        object[newPlant] = 0;
+        this.setState({object});
     }
-
+    
     render() {
         return(
             <Aux>
-                <PlantsList plants={this.state.plants}></PlantsList>
-                <PlantsControls addPlant={this.addPlantHandler}></PlantsControls>
+                <div>
+                {
+                // Key is unique plant name
+                Object.keys(this.state.plants).map( key => {
+                    return <Plant key={key} plantName={key} date={this.state.plants[key]}></Plant>
+                })}</div>
+
+                 <input ref="input"/>
+                 <button onClick={this.addPlantHandler}>Add Plant</button><br /><br />
             </Aux>
         )
     }  
